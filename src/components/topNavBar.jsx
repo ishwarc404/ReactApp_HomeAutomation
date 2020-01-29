7665b38b686fed9c7e6ca971fe5c53df7d78b75b
Ishwar Choudhary <ishwar@github.com>
2020-01-29 12:18:31 +0530

    ui updates

--- src/components/topNavBar.jsx
+++ src/components/topNavBar.jsx
@@ -16,23 +16,19 @@ class NavigationBar extends Component {
     let centre_class = "d-flex justify-content-center";
     let badge1_class = "badge m-2 badge-primary ";
     let badge2_class = "badge m-2 badge-dark ";
-    let addnewdevicebutton_class = "btn  m-2 btn-warning";
-    let importdevicebutton_class = "btn  m-2 btn-success";
+    let addnewdevicebutton_class = "btn  m-2 btn-dark";
+    let importdevicebutton_class = "btn  m-2 btn-dark";
     return (
-      <React.Fragment>
-        <div className={centre_class}>
-          <span style={{ width: 800 }} className={badge1_class}>
-            <h2 style={{ fontSize: 50 }} className={centre_class}>
+      <div>
+        <div>
+            <h2 style={{ fontSize: 50 , color:"white", fontFamily:"Helvetica Neue"}} className={centre_class}>
               Home Automation System
             </h2>
-          </span>
         </div>
-        <div className={centre_class}>
-          <span style={{ width: 400 }} className={badge2_class}>
-            <h2 style={{ fontSize: 30 }} className={centre_class}>
+        <div>
+            <h2 style={{ fontSize: 30 , color:"#e6e6e6", fontFamily:"Helvetica Neue"}} className={centre_class}>
               Devices Connected
             </h2>
-          </span>
         </div>
         <div className={centre_class}>
           <button
@@ -65,7 +61,7 @@ class NavigationBar extends Component {
             ></DevicesConnected>
           ))}
         </div>
-      </React.Fragment>
+      </div>
     );
   }
 
